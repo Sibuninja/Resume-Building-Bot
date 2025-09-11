dc711ed1b7112450cfe4b1bdd9aa37d9492303e7
Shivansh Dhyani <shivansh.dhyani.12345@gmail.com>
2025-09-11 20:53:01 +0530

    Render Ports Granted

--- app.py
+++ app.py
@@ -655,5 +655,6 @@ def generate_resume():
         return send_file(html_file, as_attachment=True, download_name=os.path.basename(html_file))
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT
+    app.run(host="0.0.0.0", port=port, debug=True)
